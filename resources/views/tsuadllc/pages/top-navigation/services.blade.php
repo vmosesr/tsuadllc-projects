c2b5488710f17e47df8ae58943ef394b2b83c756
JOSEPHAT (M) NDUNGURU <ndungurujosephat30@gmail.com>
2025-05-11 00:14:41 +0300

    Create services.blade.php

---
+++ resources/views/tsuadllc/pages/top-navigation/services.blade.php
@@ -0,0 +1,9 @@
+@include ('tsuadllc.pages.includes.header-navigation')
+
+    <main class="container p-10 d-flex justify-content-center">
+
+        <h1 class="bg-info text-white p-2">SERVICES</h1>
+        
+    </main>
+
+@include ('tsuadllc.pages.includes.footer')
