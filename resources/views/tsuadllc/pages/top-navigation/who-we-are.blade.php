0ba36b5dd1a8a4f83d3aa09a4847c29e60c58519
JOSEPHAT (M) NDUNGURU <ndungurujosephat30@gmail.com>
2025-05-23 08:01:06 +0300

    Update who-we-are.blade.php

--- resources/views/tsuadllc/pages/top-navigation/who-we-are.blade.php
+++ resources/views/tsuadllc/pages/top-navigation/who-we-are.blade.php
@@ -11,7 +11,7 @@
                         <p class="mt-4 mb-4 lead">Pioneering ethical standards for artificial intelligence development and deployment in collaboration with TSUAD Support in ICT.</p>
                         <div class="d-flex flex-wrap mt-5 who-we-are-custom">
                             <button class="btn btn-lg px-4 py-3 me-3 mb-3 bg-primary-custom text-white"><a href="{{ route('learn-more') }}" class="" style="text-decoration: none; color: white;">Learn More</a></button>
-                            <button class="who-we-are btn btn-lg px-4 py-3 mb-3 btn-outline-light"><a href="{{ route('joining-initiative') }}" class="text-white" style="text-decoration: none;">Join Our Initiative</a></button>
+                            <button class="who-we-are btn btn-lg px-4 py-3 mb-3 btn-outline-secondary"><a href="{{ route('joining-initiative') }}" class="text-white" style="text-decoration: none;">Join Our Initiative</a></button>
                         </div>
                     </div>
                     <div class="col-lg-5" data-aos="fade-left" data-aos-duration="1000">
