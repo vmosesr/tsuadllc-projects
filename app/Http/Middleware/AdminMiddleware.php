c3b19989472255d741dffa52b99009a00090fb67
JOSEPHAT (M) NDUNGURU <ndungurujosephat30@gmail.com>
2025-05-23 19:27:38 +0300

    Create AdminMiddleware.php

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Enums\UserRole;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(!\Illuminate\Support\Facades\Auth::check() || \Illuminate\Support\Facades\Auth::user()->role != UserRole::Admin){
+            return redirect()->route("home")->with('error', 'You do not have admin access.');
+        }
+
+        return $next($request);
+    }
+}
